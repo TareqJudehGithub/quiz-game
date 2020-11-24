0ed052910720e4da61f77575e58d64f3179958bd
Tareq Judeh <tareq.judeh@outlook.com>
2020-11-24 21:10:04 +0200

    Added a while loop after quiz is completed, giving the user an option to restart the quiz.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 # Practice again .. Because practice makes best!
 from question_model import Question
 from data import question_data
-from quiz_brain import AskTheBrain
+from quiz_brain import QuizBrain
 
 q_list = list()
+
 
 def add_questions_to_list():
     for i in question_data:
@@ -17,9 +18,23 @@ def add_questions_to_list():
 
 
 add_questions_to_list()
+quiz = QuizBrain(q_list)
 
-quiz = AskTheBrain(q_list)
-quiz.next_q()
+while quiz.still_has_questions():
+    quiz.next_q()
+
+    if not quiz.still_has_questions():
+        restart_quiz = input("restart_quiz? (y/n): ").lower()
+        if restart_quiz == "y":
+            quiz.q_number = 0
+            quiz.score = 0
+        else:
+
+            print(f'''You've completed the quiz.
+            Your final score is {quiz.score}.
+
+            ''')
+
 
 
 
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,22 +1,26 @@
 class QuizBrain:
-    def __init__(self, question_list):
-        self.question_number = 0
-        self.question_list = question_list
-
-# TODO: Asking the question:
-    def next_question(self):
-        current_question = self.question_list[self.question_number]
-        self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False) ")
-
-
-class AskTheBrain:
     def __init__(self, q_list):
         self.q_number = 0
         self.q_list = q_list
+        self.score = 0
+
+    def still_has_questions(self):
+        return len(self.q_list) > self.q_number
 
     def next_q(self):
         current_q = self.q_list[self.q_number]
         self.q_number += 1
-        input(f"Q.{self.q_number}: {current_q.text} (True/False)")
+        user_answer = input(f"Q.{self.q_number}: {current_q.text} (True/False) ")
+        self.check_answer(user_answer, current_q.answer)
+        print("")
 
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+            print(f"Score: {self.score}")
+        else:
+            print("That's wrong.")
+            print(f"Score: {self.score}")
+        print(f"The correct answer is: {correct_answer}")
+        print("\n")
